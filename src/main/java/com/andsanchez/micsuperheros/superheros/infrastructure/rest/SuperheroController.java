7e389f39178289894e4edc10234cac711240b284
Andrés Sánchez Rodríguez <andressanchezrodriguez@gmail.com>
2023-10-13 00:54:41 +0200

    TDD - Add integration tests scenarios for all operations with the minimum implementation

--- src/main/java/com/andsanchez/micsuperheros/superheros/infrastructure/rest/SuperheroController.java
+++ src/main/java/com/andsanchez/micsuperheros/superheros/infrastructure/rest/SuperheroController.java
@@ -1,40 +1,58 @@
 package com.andsanchez.micsuperheros.superheros.infrastructure.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class SuperheroController implements SuperheroApi {
 
+
     @Override
-    public ResponseEntity<List<SuperheroDto>> getSuperheros() {
-        return SuperheroApi.super.getSuperheros();
+    public ResponseEntity<List<SuperheroDto>> getSuperheros(String name) {
+        // TODO Implementa la lógica para obtener los superheros o buscar por nombre
+        // Implementación temporal con datos fictios
+        List<SuperheroDto> superheros = new ArrayList<>();
+        superheros.add(new SuperheroDto(1L, "Superman"));
+        if ("man".equals(name)){
+            return new ResponseEntity<>(superheros, HttpStatus.OK);
+        }
+        superheros.add(new SuperheroDto(2L, "Joker"));
+        return new ResponseEntity<>(superheros, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<SuperheroDto> createSuperhero(SuperheroDto superheroDto) {
-        return SuperheroApi.super.createSuperhero(superheroDto);
+        // TODO Implementa la lógica para crear un nuevo superhéroe a partir de superheroDto
+        // Implementación temporal que simplemente devuelve el mismo objeto recibido
+        return new ResponseEntity<>(superheroDto, HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<Void> deleteSuperhero(Long id) {
-        return SuperheroApi.super.deleteSuperhero(id);
+        // TODO Implementa la lógica para eliminar un superhéroe por su ID
+        // Implementación temporal que no realiza ninguna acción
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Override
-    public ResponseEntity<List<SuperheroDto>> findSuperheros(String name) {
-        return SuperheroApi.super.findSuperheros(name);
-    }
 
     @Override
     public ResponseEntity<SuperheroDto> getSuperheroById(Long id) {
-        return SuperheroApi.super.getSuperheroById(id);
+        // TODO Implementa la lógica para obtener un superhéroe por su ID
+        // Implementación temporal que devuelve un superhéroe ficticio por ID
+        if (id == 1L) {
+            return new ResponseEntity<>(new SuperheroDto(1L, "Superman"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
     public ResponseEntity<SuperheroDto> updateSuperhero(Long id, SuperheroDto superheroDto) {
-        return SuperheroApi.super.updateSuperhero(id, superheroDto);
+        // TODO Implementa la lógica para actualizar un superhéroe por su ID con los datos proporcionados en superheroDto
+        // Implementación temporal que simplemente devuelve el mismo objeto recibido
+        return new ResponseEntity<>(superheroDto, HttpStatus.OK);
     }
 }
--- src/test/java/com/andsanchez/micsuperheros/superheros/infrastructure/rest/SuperheroControllerIT.java
+++ src/test/java/com/andsanchez/micsuperheros/superheros/infrastructure/rest/SuperheroControllerIT.java
@@ -4,20 +4,88 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SuperheroControllerIT {
+public class SuperheroControllerIT {
 
     @Autowired
     private TestRestTemplate template;
 
     @Test
     public void getSuperheros() {
-        ResponseEntity<String> response = template.getForEntity("/v1/superheros/", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_IMPLEMENTED);
+        ResponseEntity<SuperheroDto[]> response = template.getForEntity("/v1/superheros", SuperheroDto[].class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(List.of(response.getBody()))
+                .hasSize(2)
+                .extracting("name")
+                .containsExactly("Superman", "Joker");
     }
+
+    @Test
+    public void getSuperherosByName() {
+        String nameToSearch = "man";
+        String url = "/v1/superheros?name=" + nameToSearch;
+
+        ResponseEntity<SuperheroDto[]> response = template.getForEntity(url, SuperheroDto[].class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(List.of(response.getBody()))
+                .hasSize(1)
+                .first()
+                .returns("Superman", SuperheroDto::getName);
+    }
+
+    @Test
+    public void createSuperhero() {
+        // Preparar los datos para crear un superhéroe
+        SuperheroDto superheroDto = new SuperheroDto();
+
+        ResponseEntity<String> response = template.postForEntity("/v1/superheros", superheroDto, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    public void deleteSuperhero() {
+        // Supongamos que tienes un ID válido para eliminar un superhéroe
+        Long superheroIdToDelete = 1L; // Reemplaza con un ID válido
+
+        ResponseEntity<String> response = template.exchange("/v1/superheros/{id}", HttpMethod.DELETE, null, String.class, superheroIdToDelete);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    public void getSuperheroById() {
+        Long superheroId = 1L;
+
+        ResponseEntity<SuperheroDto> response = template.getForEntity("/v1/superheros/{id}", SuperheroDto.class, superheroId);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull()
+                .returns(1L, SuperheroDto::getId)
+                .returns("Superman", SuperheroDto::getName);
+    }
+
+    @Test
+    public void updateSuperhero() {
+        Long superheroIdToUpdate = 1L;
+        SuperheroDto updatedSuperheroDto = new SuperheroDto();
+
+        ResponseEntity<String> response = template.exchange("/v1/superheros/{id}", HttpMethod.PUT, new HttpEntity<>(updatedSuperheroDto), String.class, superheroIdToUpdate);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
 }
